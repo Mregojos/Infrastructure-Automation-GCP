8dd6c0c99b5ae3ec9462914909873c4a37ffee3e
Matt <mattiregojos@gmail.com>
2023-11-12 07:42:15 +0000

    Add and modify files

---
+++ sh-files/.ipynb_checkpoints/automation-checkpoint.sh
@@ -0,0 +1,13 @@
+# Create a compute engine instance
+    # No Service Account
+# Download container
+# Create a database by running run database image
+# Create a static external ip address
+# Use the static address to the compute engine
+# Use these credentials to connect to the database
+
+# App in a repository
+# Build an image
+# Submit it to the  the Artifact Registry
+# Create a service account to be used
+# Deploy using Cloud Run
