97634a77befde09ded0a08e8ea1b993008e0d79d
Matt <mattiregojos@gmail.com>
2023-11-13 11:41:17 +0000

    Add and modify files

--- sh-files/clean-up.sh
+++ sh-files/clean-up.sh
@@ -1,3 +1,18 @@
+# Environment Variables
+export DB_NAME="db"
+export MACHINE_TYPE="e2-micro"
+export REGION="us-west1"
+export ZONE="us-west1-a"
+export BOOT_DISK_SIZE="30"
+export TAGS="db"
+export FIREWALL_RULES_NAME="ports"
+export STATIC_IP_ADDRESS_NAME="db-static-ip-address"
+export CLOUD_BUILD_REGION="us-west2"
+export APP_ARTIFACT_NAME="app"
+export APP_NAME="app"
+export APP_VERSION="latest"
+export APP_SERVICE_ACCOUNT_NAME='app-service-account'
+echo "#----------Exporting Environment Variables is done.----------#"
 gcloud compute instances delete $DB_NAME --zone=$ZONE --quiet
 gcloud compute addresses delete $STATIC_IP_ADDRESS_NAME --region $REGION --quiet
 gcloud compute firewall-rules delete $FIREWALL_RULES_NAME --quiet
