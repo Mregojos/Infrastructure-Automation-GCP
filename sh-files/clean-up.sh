612b62994efb904f42c86896ac28d5103f7444d5
Matt <mattiregojo@gmail.com>
2023-11-14 01:31:51 +0000

    Add and modify files

--- sh-files/clean-up.sh
+++ sh-files/clean-up.sh
@@ -12,10 +12,18 @@ export APP_ARTIFACT_NAME="app"
 export APP_NAME="app"
 export APP_VERSION="latest"
 export APP_SERVICE_ACCOUNT_NAME='app-service-account'
-echo "#----------Exporting Environment Variables is done.----------#"
+export BUCKET_NAME='matt-startup-script'
+export STARTUP_SCRIPT_BUCKET_SA='startup-script-bucket-sa'
+export STARTUP_SCRIPT_NAME='startup-script.sh'
+echo "\n #----------Exporting Environment Variables is done.----------# \n"
+
 gcloud compute instances delete $DB_NAME --zone=$ZONE --quiet
 gcloud compute addresses delete $STATIC_IP_ADDRESS_NAME --region $REGION --quiet
 gcloud compute firewall-rules delete $FIREWALL_RULES_NAME --quiet
 gcloud artifacts repositories delete $APP_ARTIFACT_NAME --location=$REGION --quiet
 gcloud run services delete $APP_NAME --region=$REGION --quiet
 gcloud iam service-accounts delete $APP_SERVICE_ACCOUNT_NAME@$(gcloud config get project).iam.gserviceaccount.com --quiet
+gcloud iam service-accounts delete $APP_SERVICE_ACCOUNT_NAME@$(gcloud config get project).iam.gserviceaccount.com --quiet
+gcloud storage rm -r gs://$BUCKET_NAME
+gcloud storage rm -r gs://$(gcloud config get project)_cloudbuild
+echo "\n #----------Services and Resources have been Successfully deleted.----------# \n"
