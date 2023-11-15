ba4624351a11613d1c3f72ed6c4ef4a842b68cad
Matt <mattiregojos@gmail.com>
2023-11-15 02:36:04 +0000

    Add and modify files

--- app-model/local-development.sh
+++ app-model/local-development.sh
@@ -5,8 +5,10 @@
 
 # Environment Variables
 APP_NAME="app"
+# APP_NAME="simple-app"
 FIREWALL_RULES_NAME="ports"
 
+# For App
 # Build
 docker build -t $APP_NAME .
 # Run
@@ -16,3 +18,25 @@ gcloud compute --project=$(gcloud config get project) firewall-rules create $FIR
     --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:5000,tcp:8000,tcp:9000 --source-ranges=0.0.0.0/0 
 # Remove docker container
 # docker rm -f $APP_NAME
+
+# Database
+# With volume/data connected
+docker run -d \
+    --name postgres-sql \
+    -e POSTGRES_USER=matt \
+    -e POSTGRES_PASSWORD=password \
+    -v $(pwd)/data/:/var/lib/postgresql/data/ \
+    -p 5000:5432 \
+    postgres
+docker run -p 8000:80 \
+    -e 'PGADMIN_DEFAULT_EMAIL=matt@example.com' \
+    -e 'PGADMIN_DEFAULT_PASSWORD=password' \
+    -d dpage/pgadmin4
+
+# Environment Variables for the app
+DBNAME: "matt"
+USER: "matt"
+HOST: ""
+DBPORT: "5000"
+DBPASSWORD: "password"
+PROJECT_NAME: ""
--- app-model/simple-app.py
+++ app-model/simple-app.py
@@ -3,5 +3,8 @@ import os
 
 Matt=os.getenv("Matt")
 st.text(Matt)
+
+st.text(Matt)
+st.text(Matt)
 Mat=os.getenv("Mat")
 st.text(Mat)
