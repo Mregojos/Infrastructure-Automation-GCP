16e0b037f1be057b9fc7c8ca04f268db3ef35282
Matt <mattiregojos@gmail.com>
2023-11-24 07:40:31 +0000

    Add and modify files

--- synch.sh
+++ synch.sh
@@ -1,3 +1,3 @@
 # In model-deployment App Repository run this to update the app and the deployment
-cat app-deployment.sh >> ~/infrastructure-automation-gcp/infrastructure-automation-gcp.sh
+cp app-deployment.sh ~/infrastructure-automation-gcp/infrastructure-automation-gcp.sh
 cp -rf app-deployment/* ~/infrastructure-automation-gcp/app
