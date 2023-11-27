7a2c6f3842d5e79a305f1e305be081666aefb383
Matt <mattiregojos@gmail.com>
2023-11-27 04:29:37 +0000

    Add and modify files

--- tf-files/tf-cleanup.sh
+++ tf-files/tf-cleanup.sh
@@ -1,8 +1,8 @@
+gcloud compute firewall-rules delete $FIREWALL_RULES_NAME --quiet
+gcloud run services delete $APP_NAME --region=$REGION --quiet
+terraform destroy
 rm -rf t*.tfstate
 rm -rf t*.*.backup
 rm -rf main.tf
 rm -rf .t*.hcl
 rm -rf .i*
-gcloud compute firewall-rules delete $FIREWALL_RULES_NAME --quiet
-gcloud run services delete $APP_NAME --region=$REGION --quiet
-terraform destroy
