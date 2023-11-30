0d6b2a72fae694b480c1847f2bd7f8c704e0684c
Matt <mattiregojos@gmail.com>
2023-11-30 22:53:27 +0000

    Add and modify files

--- cleanup.sh
+++ cleanup.sh
@@ -1,61 +1,3 @@
-#---------Application Name Environment Variables----------#
-VERSION="i"
-APP_NAME="infra-auto-$VERSION"
-
-#---------Project Environment Variables---------#
-PROJECT_NAME="$(gcloud config get project)"
-
-#----------Database Instance Environment Variables----------#
-VPC_NAME="$APP_NAME-vpc"
-SUBNET_NAME="$APP_NAME-subnet"
-RANGE_A='10.100.0.0/20'
-RANGE_B='10.200.0.0/20'
-DB_INSTANCE_NAME="$APP_NAME-db"
-MACHINE_TYPE="e2-micro"
-REGION="us-west1"
-ZONE="us-west1-a"
-BOOT_DISK_SIZE="30"
-TAGS="db"
-FIREWALL_RULES_NAME="$APP_NAME-ports"
-STATIC_IP_ADDRESS_NAME="db-static-ip-address"
-BUCKET_NAME="$APP_NAME-startup-script"
-STARTUP_SCRIPT_BUCKET_SA="startup-script-bucket-sa"
-STARTUP_SCRIPT_BUCKET_CUSTOM_ROLE="bucketCustomRole.$VERSION"
-# STARTUP_SCRIPT_NAME="$APP_NAME-startup-script.sh"
-
-# For Notebook 
-NOTEBOOK_REGION='us-central1'
-RANGE_C='10.150.0.0/20'
-
-#---------Database Credentials----------#
-DB_CONTAINER_NAME="$APP_NAME-postgres-sql"
-DB_NAME="$APP_NAME-admin"
-DB_USER="$APP_NAME-admin" 
-# DB_HOST=$(gcloud compute addresses describe $STATIC_IP_ADDRESS_NAME --region $REGION | grep "address: " | cut -d " " -f2)
-DB_HOST=$(gcloud compute instances list --filter="name=$DB_INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)") 
-DB_PORT=5000
-DB_PASSWORD=$APP_NAME 
-PROJECT_NAME="$(gcloud config get project)"
-ADMIN_PASSWORD=$APP_NAME 
-APP_PORT=9000
-APP_ADDRESS=""
-DOMAIN_NAME=""
-SPECIAL_NAME=$APP_NAME 
-
-#----------Deployment Environment Variables----------#
-CLOUD_BUILD_REGION="us-west2"
-REGION="us-west1"
-APP_ARTIFACT_NAME="$APP_NAME-artifact-registry"
-APP_VERSION="latest"
-APP_SERVICE_ACCOUNT_NAME="app-service-account"
-APP_CUSTOM_ROLE="appCustomRole.$VERSION"
-APP_PORT=9000
-APP_ENV_FILE=".env.yaml"
-MIN_INSTANCES=1
-MAX_INSTANCES=1
-
-echo "\n #----------Exporting Environment Variables is done.----------# \n"
-
 #----------Delete Resources----------#
 gcloud compute instances delete $DB_INSTANCE_NAME --zone=$ZONE --quiet
 gcloud compute addresses delete $STATIC_IP_ADDRESS_NAME --region $REGION --quiet
@@ -126,3 +68,5 @@ gcloud compute networks subnets delete $SUBNET_NAME-$NOTEBOOK_REGION --region=$N
 gcloud compute networks delete $VPC_NAME --quiet
 
 echo "\n #----------Custom Roles have been Successfully deleted.----------# \n"
+
+echo "\n #----------DONE----------# \n"
---
+++ environment-variables.sh
@@ -0,0 +1,57 @@
+#---------Application Name Environment Variables----------#
+source VERSION="i"
+source APP_NAME="infra-auto-$VERSION"
+
+#---------Project Environment Variables---------#
+source PROJECT_NAME="$(gcloud config get project)"
+
+#----------Database Instance Environment Variables----------#
+source VPC_NAME="$APP_NAME-vpc"
+source SUBNET_NAME="$APP_NAME-subnet"
+source RANGE_A='10.100.0.0/20'
+source RANGE_B='10.200.0.0/20'
+source DB_INSTANCE_NAME="$APP_NAME-db"
+source MACHINE_TYPE="e2-micro"
+source REGION="us-west1"
+source ZONE="us-west1-a"
+source BOOT_DISK_SIZE="30"
+source TAGS="db"
+source FIREWALL_RULES_NAME="$APP_NAME-ports"
+source STATIC_IP_ADDRESS_NAME="db-static-ip-address"
+source BUCKET_NAME="$APP_NAME-startup-script"
+source STARTUP_SCRIPT_BUCKET_SA="startup-script-bucket-sa"
+source STARTUP_SCRIPT_BUCKET_CUSTOM_ROLE="bucketCustomRole.$VERSION"
+# source STARTUP_SCRIPT_NAME="$APP_NAME-startup-script.sh"
+
+# For Notebook 
+source NOTEBOOK_REGION='us-central1'
+source RANGE_C='10.150.0.0/20'
+
+#---------Database Credentials----------#
+source DB_CONTAINER_NAME="$APP_NAME-postgres-sql"
+source DB_NAME="$APP_NAME-admin"
+source DB_USER="$APP_NAME-admin" 
+# source DB_HOST=$(gcloud compute addresses describe $STATIC_IP_ADDRESS_NAME --region $REGION | grep "address: " | cut -d " " -f2)
+source DB_HOST=$(gcloud compute instances list --filter="name=$DB_INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)") 
+source DB_PORT=5000
+source DB_PASSWORD=$APP_NAME 
+source PROJECT_NAME="$(gcloud config get project)"
+source ADMIN_PASSWORD=$APP_NAME 
+source APP_PORT=9000
+source APP_ADDRESS=""
+source DOMAIN_NAME=""
+source SPECIAL_NAME=$APP_NAME 
+
+#----------Deployment Environment Variables----------#
+source CLOUD_BUILD_REGION="us-west2"
+source REGION="us-west1"
+source APP_ARTIFACT_NAME="$APP_NAME-artifact-registry"
+source APP_VERSION="latest"
+source APP_SERVICE_ACCOUNT_NAME="app-service-account"
+source APP_CUSTOM_ROLE="appCustomRole.$VERSION"
+source APP_PORT=9000
+source APP_ENV_FILE=".env.yaml"
+source MIN_INSTANCES=1
+source MAX_INSTANCES=1
+
+echo "\n #----------Exporting Environment Variables is done.----------# \n"
