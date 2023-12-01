5a47f015826a56ddc6fb078c84d526b1197ab45d
Matt <mattiregojos@gmail.com>
2023-12-01 02:17:10 +0000

    Add and modify files

--- tf-files-with-state/tf-state-gcp.sh
+++ tf-files-with-state/tf-state-gcp.sh
@@ -7,34 +7,52 @@ echo "\n #----------Services have been successfully enabled.----------# \n"
 # sh tf-main.sh
 
 # With variables
-sh tf-vars-main.sh
+sh tf-state-vars-main.sh
 
-cat >> terraform.tfvars << EOF
-tf_bucket_backend = "$APP_NAME-tf-bucket-backend"
-EOF
+cd app
 
-cat >> variables.tf << EOF
-variable "tf_bucket_backend"
-EOF
+# build and submnit an image to Artifact Registry
+gcloud builds submit \
+    --region=$CLOUD_BUILD_REGION \
+    --tag $REGION-docker.pkg.dev/$(gcloud config get-value project)/$APP_ARTIFACT_NAME/$APP_NAME:$APP_VERSION
+echo "\n #----------Docker image has been successfully built.----------# \n"
 
-cat > bucket-backend.tf << EOF
-resource "google_storage_bucket" "tf_bucket_backend" {
-    name = var.tf_bucket_bakend
-    force_destroy = false
-    location = "US"
-    storage_class = "STANDARD"
-    versioning {
-      enabled = true
-    }
-}
-EOF
+# DB_INSTANCE_NAME Address / Host
+DB_HOST=$(gcloud compute instances list --filter="name=$DB_INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)") 
+
+# Environment Variables for the app
+echo """
+DB_NAME:
+    '$DB_NAME'
+DB_USER:
+    '$DB_USER'
+DB_HOST:
+    '$DB_HOST'
+DB_PORT:
+    '$DB_PORT'
+DB_PASSWORD:
+    '$DB_PASSWORD'
+PROJECT_NAME:
+    '$PROJECT_NAME'
+ADMIN_PASSWORD:
+    '$ADMIN_PASSWORD'
+APP_PORT:
+    '$APP_PORT'
+APP_ADDRESS:
+    '$APP_ADDRESS'
+DOMAIN_NAME:
+    '$DOMAIN_NAME'
+SPECIAL_NAME:
+    '$SPECIAL_NAME'
+""" > .env.yaml
 
 
-cat > backend.tf << EOF
-terraform {
-    bakend "gcs" {
-        bucket = var.tf_bucket_bakend
-        prefix = "terraform/state"
-    }
-}
-EOF
+# Deploy the app using Cloud Run
+gcloud run deploy $APP_NAME \
+    --max-instances=$MAX_INSTANCES --min-instances=$MIN_INSTANCES --port=$APP_PORT \
+    --env-vars-file=$APP_ENV_FILE \
+    --image=$REGION-docker.pkg.dev/$(gcloud config get project)/$APP_ARTIFACT_NAME/$APP_NAME:$APP_VERSION \
+    --allow-unauthenticated \
+    --region=$REGION \
+    --service-account=$APP_SERVICE_ACCOUNT_NAME@$(gcloud config get project).iam.gserviceaccount.com 
+echo "\n #----------The application has been successfully deployed.----------# \n"
---
+++ tf-files-with-state/tf-state-vars-main.sh
@@ -0,0 +1,204 @@
+cat > terraform.tfvars << EOF
+tf_project = "$PROJECT_NAME"
+tf_vpc_name = "$VPC_NAME"
+tf_bucket_name = "$BUCKET_NAME"
+tf_role = "projects/$(gcloud config get project)/roles/$STARTUP_SCRIPT_BUCKET_CUSTOM_ROLE"
+tf_member = "serviceAccount:$STARTUP_SCRIPT_BUCKET_SA@$(gcloud config get project).iam.gserviceaccount.com"
+tf_metadata_startup_script = "gcloud storage cp gs://$BUCKET_NAME/startup-script.sh . \n sh startup-script.sh"
+tf_email = "$STARTUP_SCRIPT_BUCKET_SA@$(gcloud config get project).iam.gserviceaccount.com"
+tf_app_role = "projects/$(gcloud config get project)/roles/$APP_CUSTOM_ROLE"
+tf_app_member = "serviceAccount:$APP_SERVICE_ACCOUNT_NAME@$(gcloud config get project).iam.gserviceaccount.com"
+tf_bucket_backend = "$APP_NAME-tf-bucket-backend"
+EOF
+
+cat > variables.tf << EOF
+variable "tf_project" {}
+variable "tf_region" {
+    default = "northamerica-northeast1"
+}
+variable "tf_zone" {
+    default = "northamerica-northeast1-a"
+}
+variable "tf_vpc_name" {}
+variable "tf_bucket_name" {}
+variable "tf_role" {}
+variable "tf_member" {}
+variable "tf_metadata_startup_script" {}
+variable "tf_email" {}
+variable "tf_app_role" {}
+variable "tf_app_member" {}
+variable "tf_bucket_backend"
+EOF
+
+cat > main.tf << EOF
+# Provider
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.tf_project
+  region  = var.tf_region
+  zone    = var.tf_zone
+}
+
+# Create VPC Network resource
+resource "google_compute_network" "tf_vpc" {
+  name = var.tf_vpc_name
+  auto_create_subnetworks = false
+  mtu = 1460
+}
+
+# Create three subnets
+resource "google_compute_subnetwork" "$REGION" {
+  name          = "$SUBNET_NAME-$REGION"
+  ip_cidr_range = "$RANGE_A"
+  region        = "$REGION"
+  network       = google_compute_network.tf_vpc.id
+}
+
+resource "google_compute_subnetwork" "$CLOUD_BUILD_REGION" {
+  name          = "$SUBNET_NAME-$CLOUD_BUILD_REGION"
+  ip_cidr_range = "$RANGE_B"
+  region        = "$CLOUD_BUILD_REGION"
+  network       = google_compute_network.tf_vpc.id
+}
+
+resource "google_compute_subnetwork" "$NOTEBOOK_REGION" {
+  name          = "$SUBNET_NAME-$NOTEBOOK_REGION"
+  ip_cidr_range = "$RANGE_C"
+  region        = "$NOTEBOOK_REGION"
+  network       = google_compute_network.tf_vpc.id
+}
+
+resource "google_compute_address" "$STATIC_IP_ADDRESS_NAME" {
+    name = "$STATIC_IP_ADDRESS_NAME"
+    region = "$REGION"
+}
+
+resource "google_storage_bucket" "tf_bucket" {
+    name = var.tf_bucket_name
+    location = "US"
+    force_destroy = true
+}
+
+resource "google_storage_bucket_object" "startup-script-object" {
+    name = "$STARTUP_SCRIPT_NAME.sh"
+    source = "app/$STARTUP_SCRIPT_NAME.sh"
+    bucket = google_storage_bucket.tf_bucket.id
+}
+
+resource "google_service_account" "$STARTUP_SCRIPT_BUCKET_SA" {
+    account_id = "$STARTUP_SCRIPT_BUCKET_SA"
+    display_name = "$STARTUP_SCRIPT_BUCKET_SA"
+}
+
+resource "google_project_iam_custom_role" "BUCKET_CUSTOM_ROLE" {
+    role_id = "$STARTUP_SCRIPT_BUCKET_CUSTOM_ROLE"
+    title = "$STARTUP_SCRIPT_BUCKET_CUSTOM_ROLE"
+    description = "Get the object only"
+    permissions = ["storage.objects.get"]
+}
+
+resource "google_project_iam_binding" "BUCKET_BINDING" {
+    project = var.tf_project
+    role = var.tf_role
+    members = [
+        var.tf_member
+        ]
+}
+
+resource "google_compute_instance" "$DB_INSTANCE_NAME" {
+    name = "$DB_INSTANCE_NAME"
+    machine_type = "$MACHINE_TYPE"
+    zone = "$ZONE"
+    tags = ["$TAGS"]
+    boot_disk {
+        initialize_params {
+        image = "debian-cloud/debian-11"
+        }
+    }
+    network_interface {
+        network = var.tf_vpc_name
+        subnetwork = google_compute_subnetwork.$REGION.name
+        access_config {
+            nat_ip = google_compute_address.$STATIC_IP_ADDRESS_NAME.address
+        }
+    }
+    metadata_startup_script = var.tf_metadata_startup_script
+    service_account {
+        email = var.tf_email
+        scopes = ["cloud-platform"]
+    }
+    
+}
+
+resource "google_artifact_registry_repository" "$APP_ARTIFACT_NAME" {
+    location = "$REGION"
+    repository_id = "$APP_ARTIFACT_NAME"
+    description = "Docker repository"
+    format = "DOCKER"
+}
+
+resource "google_service_account" "$APP_SERVICE_ACCOUNT_NAME" {
+    account_id = "$APP_SERVICE_ACCOUNT_NAME"
+    display_name = "$APP_SERVICE_ACCOUNT_NAME"
+}
+
+resource "google_project_iam_custom_role" "APP_CUSTOM_ROLE" {
+    role_id = "$APP_CUSTOM_ROLE"
+    title = "$APP_CUSTOM_ROLE"
+    description = "Predict Only"
+    permissions = ["aiplatform.endpoints.predict"]
+}
+
+resource "google_project_iam_binding" "APP_BINDING" {
+    project = var.tf_project
+    role = var.tf_app_role
+    members = [
+        var.tf_app_member
+        ]
+}
+
+resource "google_compute_firewall" "$FIREWALL_RULES_NAME" {
+    name = "$FIREWALL_RULES_NAME"
+    allow {
+      ports = ["5000", "8000"]
+      protocol = "tcp"
+    }
+    direction = "INGRESS"
+    network = google_compute_network.tf_vpc.name
+    priority = 1000
+    source_ranges = ["0.0.0.0/0"]
+    target_tags = ["$TAGS"]
+}
+
+resource "google_storage_bucket" "tf_bucket_backend" {
+    name = var.tf_bucket_backend
+    force_destroy = false
+    location = "US"
+    storage_class = "STANDARD"
+    versioning {
+      enabled = true
+    }
+}
+
+EOF
+
+cat > backend.tf << EOF
+terraform {
+    bakend "gcs" {
+        bucket = var.tf_bucket_backend
+        prefix = "terraform/state"
+    }
+}
+EOF
+
+sh tf.sh
+
+echo "\n GCP Services successful created. \n"
